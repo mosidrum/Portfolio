6fad124abdd9a9bc53646ee4b02c27b85e0ce66d
mosidrum <mosiokanga@gmail.com>
2023-04-12 12:15:42 +0100

    Validate email to lowercase

--- index.js
+++ index.js
@@ -15,3 +15,44 @@ function closeMenu () {
     menu.style.display = 'flex';
   }
 }
+
+function message () {
+ 
+  
+  const email = document.getElementById('email')
+  
+
+
+  if (name.value === '' || email.value === '' || msg.value === ''){
+    danger.style.display = 'block'
+  } else {
+    setTimeout(()=> {
+      name.value = ''
+      email.value = ''
+      msg.value = ''
+    }, 2000)
+    success.style.display = 'block'
+  }
+
+ 
+}
+
+function validateEmail(email){
+  var email = document.getElementById("email").value;
+  const msg = document.getElementById('textarea').value
+  const name = document.getElementById('name').value
+  const success = document.getElementById('success')
+  const danger = document.getElementById('danger')
+  
+  if (email === String(email).toLowerCase() && email != ''){
+    success.style.display = 'block'
+  } else {
+    danger.style.display = 'block'
+    setTimeout(()=>{
+      name ===''
+      email ===''
+      msg ===''
+    },2000)
+  }
+  
+}
